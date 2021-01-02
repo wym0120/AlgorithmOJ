44b9ff8c14a886a628cb4defcbfc2b2005606294
wym0120 <wym0120@github.com>
2021-01-02 16:30:03 +0800

    pass 1_6 1_7

---
+++ 1_6.cpp
@@ -0,0 +1,40 @@
+/**
+ * <------Description------>
+ * There are Infinite People Standing in a row, indexed from 1.
+ * A person having index 'i' has strength of (i*i).You have Strength 'P'.
+ * You need to tell what is the maximum number of People You can Kill With your Strength P.
+ * You can only Kill a person with strength 'X' if P >= 'X' and after killing him, Your Strength decreases by 'X'.
+ *
+ * <------Input------>
+ * First line contains an integer 'T' - the number of testcases,Each of the next 'T' lines contains an integer 'P'- Your Power.Constraints:1<=T<=100001<=P<=1000000000000000
+ *
+ * <------Output------>
+ * For each testcase Output The maximum Number of People You can kill.
+ *
+ * */
+
+#include <iostream>
+using namespace std;
+
+void CountMaxP(long p){
+    long count = 0;
+    long index = 1;
+    while (p >= 0){
+        p -= index * index;
+        index ++;
+        count ++;
+    }
+    count --;
+    cout << count << endl;
+}
+
+int main(){
+    long T,P;
+    cin >> T;
+    for (long i = 0 ; i<T;i++){
+        cin >> P;
+        CountMaxP(P);
+    }
+    return 0;
+}
+
---
+++ 1_7.cpp
@@ -0,0 +1,53 @@
+/**
+ * <------Description------>
+ * Given a Complete Binary tree, print the level order traversal in sorted order.
+ *
+ * <------Input------>
+ *
+ * The first line of the input contains integer T denoting the number of test cases.
+ * For each test case, the first line takes an integer n denoting the size of array
+ * i.e number of nodes followed by n-space separated integers denoting the nodes of the tree in level order fashion.
+ * (1<=T<=100；1<=n<=10^5)
+ *
+ * <------Output------>
+ * For each test case, the output is the level order sorted tree. ( Note: For every level, we only print distinct elements.)
+ *
+ * */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int T, N = 0;
+    cin >> T;
+
+    while (T > 0){
+        cin >> N;
+        vector<int> v;
+        while(N>0){
+            int num;
+            cin >> num;
+            v.push_back(num);
+            N--;
+        }
+        int i = 0;
+        int sum = 0;
+        while(pow(2,i)-1 < v.size()){
+            int head_offset = sum;
+            int tail_offset = sum + (int)(pow(2,i));
+
+            sort(v.begin() + head_offset,min(v.begin()+tail_offset,v.end()));
+            sum += (int)(pow(2,i));
+            i++;
+            std::vector<int>::iterator it;
+            for (it = v.begin()+head_offset; it != min(v.begin()+tail_offset,v.end()); ++it){
+                cout << ' ' << *it;
+            }
+            cout<<endl;
+        }
+        T--;
+    }
+    return 0;
+}
