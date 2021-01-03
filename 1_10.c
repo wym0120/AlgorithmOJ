81cfe82258de1694efe3dd03f70ef8bfe7aa1205
wym0120 <wym0120@github.com>
2021-01-03 23:21:08 +0800

    reformat 1_10

--- 1_10.c
+++ 1_10.c
@@ -1,3 +1,19 @@
+/**
+ * <------Description------>
+ * Given K sorted arrays arranged in a form of a matrix, your task is to merge them.
+ * You need to complete mergeKArrays function which takes 2 arguments, an arr[k][k] 2D Matrix containing k sorted arrays and an integer k denoting the no. of sorted arrays.
+ * The function should return a pointer to the merged sorted arrays. There are multiple test cases.
+ * For each test case, this method will be called individually.
+ *
+ * <------Input------>
+ * The first line of input will denote the no of Test cases then T test cases will follow .
+ * Each test cases will contain an integer N denoting the no of sorted arrays.
+ * Then in the next line contains all the elements of the array separated by space.（1<=T<=50；1<=N<=10）
+ *
+ * <------Output------>
+ * The output will be the sorted merged array.
+ * */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,7 +23,6 @@ int main() {
     scanf("%d", &T);
     getchar();
     while (T > 0) {
-        int N = 0;
         char *buffer;
         size_t bsize = 10000;
         buffer = (char *) malloc(bsize * sizeof(char));
