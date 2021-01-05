8ea7b5d6dfa1a46d4fd59c51a11a0d04a0e4ecfa
wym0120 <wym0120@github.com>
2021-01-05 22:41:41 +0800

    pass 1_15.cpp write 1_10.cpp

--- 1_5.cpp
+++ 1_5.cpp
@@ -1,29 +1,22 @@
 /**
  * <------Description------>
- * You are given N number of books.
- * Every ith book has Pi number of pages.
- * You have to allocate books to M number of students.
- * There can be many ways or permutations to do so.
- * In each permutation one of the M students will be allocated the maximum number of pages.
- * Out of all these permutations, the task is to find that particular permutation in which the maximum number of pages allocated to a student is minimum of those in all the other permutations, and print this minimum value. Each book will be allocated to exactly one student.
- * Each student has to be allocated at least one book.
- * Each Student can be allocated with contiguous books.
+ * Dilpreet wants to paint his dog- Buzo's home that has n boards with different lengths[A1, A2,..., An].
+ * He hired k painters for this work and each painter takes 1 unit time to paint 1 unit of the board.The problem is to find the minimum time to get this job done under the constraints that any painter will only paint continuous sections of boards, say board {2, 3, 4} or only board {1} or nothing but not board {2, 4, 5}.
  *
  * <------Input------>
- * The first line contains 'T' denoting the number of testcases.
- * Then follows description of T testcases:Each case begins with a single positive integer N denoting the number of books.
- * The second line contains N space separated positive integers denoting the pages of each book.
- * And the third line contains another integer M, denoting the number of studentsConstraints:1<= T <=70，1<= N <=50，1<= A [ i ] <=250，1<= M <=50，Note: Return -1 if a valid assignment is not possible, and allotment should be in contiguous order (see explanation for better understanding)
+ * The first line consists of a single integer T, the number of test cases.
+ * For each test case, the first line contains an integer k denoting the number of painters and integer n denoting the number of boards.
+ * Next line contains n- space separated integers denoting the size of boards.
+ * Constraints:1<=T<=1001<=k<=301<=n<=501<=A[i]<=500
  *
  * <------Output------>
- * For each test case, output a single line containing minimum number of pages each student has to read for corresponding test case.
+ * For each test case, the output is an integer displaying the minimum time for painting that house.
  * */
 
 #include <iostream>
 #include <vector>
 #include <numeric>
 #include <algorithm>
-#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -64,16 +57,14 @@ int main() {
     int T;
     cin >> T;
     while (T-- > 0) {
-        int num_of_books;
-        cin >> num_of_books;
+        int num_of_books, num_of_students;
+        cin >> num_of_students >> num_of_books;
         vector<int> pages;
         for (int i = 0; i < num_of_books; i++) {
             int page;
             cin >> page;
             pages.push_back(page);
         }
-        int num_of_students;
-        cin >> num_of_students;
 
         if (num_of_books < num_of_students) {
             cout << -1 << endl;
